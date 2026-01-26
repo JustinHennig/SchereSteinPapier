8ae499c3eec4b5ac42ee23fe07d2494460a198c5
JustinHennig <110170839+JustinHennig@users.noreply.github.com>
2026-01-26 13:24:47 +0100

    Implement basic GameGUI constructor and layout

--- GameGUI.java
+++ GameGUI.java
@@ -1,3 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GameGUI extends JFrame {
-    
+
+    public GameGUI() {
+        setTitle("Schere Stein Papier");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setVisible(true);
+    }
 }
